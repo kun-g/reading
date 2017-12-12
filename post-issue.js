454e0c62f6ed8aa2c07734487017e0df950d281b
Kun.G <kun.g@qq.com>
2017-12-12 17:09:44 +0800

    close #1

---
+++ post-issue.js
@@ -0,0 +1,61 @@
+var GitHub = require('github-api');
+
+// basic auth
+var gh = new GitHub({
+    token: '765134f4565d8405a8ad1c53595a8573a3488fb6'
+});
+
+var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
+//me.listNotifications(function(err, notifications) {
+//    console.log('notifications',  notifications);
+//});
+
+//me.listStarredRepos(function(err, repos) {
+//    console.log(repos);
+//   // look at all the starred repos!
+//});
+
+var issue = gh.getIssues("kun-g", "reading");
+//issue.listIssues({}, function (err, data) { console.log(data); });
+
+
+function createIssue(url) {
+    getTitle(url, function (err, title) {
+        if (err) {
+            throw err
+        }
+        issue.createIssue({
+            "title": title,
+            "body": url,
+            "assignees": [ "kun-g" ],
+            "milestone": 1,
+            //"labels": [ "bug" ]
+        }, function (err, data) {
+            console.log('Created', title);
+        });
+    });
+
+}
+
+function getTitle(url, cb) {
+    let { get } = require('http');
+
+    get(url, function (res) {
+        const { statusCode } = res;
+        if (statusCode !== 200) cb(new Error("Fail To Open"));
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            let matched = rawData.match(/<title>(.*)<\/title>/i);
+            if (matched) {
+                let [, title] = matched
+                cb(null, title);
+            } else {
+                cb(new Error("No title"));
+            }
+        });
+    });
+}
+
+createIssue('http://insights.thoughtworks.cn/agile-learning-method-base-on-github/');
